72b11624a3020b199f4f61e0e32b8e0d605a3ff7
evgenyTarasovRepo <eatarasov@yahoo.com>
2023-01-31 19:57:28 +0300

    hometask for lesson 11. create firts task.

--- src/test/java/com/tarasov/demoqaproject/pages/FormPage.java
+++ src/test/java/com/tarasov/demoqaproject/pages/FormPage.java
@@ -3,6 +3,7 @@ package com.tarasov.demoqaproject.pages;
 import com.codeborne.selenide.SelenideElement;
 import com.tarasov.demoqaproject.pages.components.CalendarComponent;
 import com.tarasov.demoqaproject.pages.components.ModalComponent;
+import io.qameta.allure.Step;
 import org.openqa.selenium.Keys;
 
 import java.util.HashMap;
@@ -31,6 +32,7 @@ public class FormPage {
     private final SelenideElement submitBtn = $("#submit");
 
 
+    @Step("Open form page")
     public FormPage openPage() {
         open("/automation-practice-form");
         $(".practice-form-wrapper").shouldHave(text("Student Registration Form"));
@@ -39,92 +41,106 @@ public class FormPage {
         return this;
     }
 
+    @Step("Set first name {firstName}")
     public FormPage setFirstName(String firstName) {
         firstNameInput.setValue(firstName);
         return this;
     }
 
+    @Step("Set lastname {lastName}")
     public FormPage setLastName(String lastName) {
         lastNameInput.setValue(lastName);
         return this;
     }
 
+    @Step("Set user email {userEmail}")
     public FormPage setUserEmail(String userEmail) {
         userEmailInput.setValue(userEmail);
         return this;
     }
 
+    @Step("Set user gender {value}")
     public FormPage setUserGender(String value) {
         userGender.$(byText(value)).click();
         return this;
     }
 
+    @Step("Set user number {number}")
     public FormPage setUserNumber(String number) {
         userNumber.setValue(number);
         return this;
     }
 
+    @Step("Set date")
     public FormPage setDate(String month, String year, String day) {
         dateOfBirthInput.click();
         calendarComponent.setDate(month, year, day);
         return this;
     }
 
+    @Step("Set subject {subject}")
     public FormPage setSubject(String subject) {
         subjectsInput.setValue(subject).pressEnter();
         return this;
     }
 
+    @Step("Set hobby {hobby}")
     public FormPage setHobby(String hobby) {
         hobbyPicker.$(byText(hobby)).click();
         return this;
     }
 
+    @Step("Set picture")
     public FormPage setPicture(String path) {
         uploadPicture.uploadFromClasspath(path);
         return this;
     }
 
+    @Step("Set address {address}")
     public FormPage setAddress(String address) {
         currentAddress.setValue(address);
         return this;
     }
 
+    @Step("Set state {state}")
     public FormPage setState(String state) {
         userState.click();
         userState.$(byText(state)).click();
         return this;
     }
 
+    @Step("Set city {city}")
     public FormPage setCity(String city) {
         userCity.click();
         userCity.$(byText(city)).click();
         return this;
     }
 
+    @Step("Press submit")
     public FormPage pressSubmitBtn() {
         submitBtn.click();
         return this;
     }
 
+    @Step("Verify modal")
     public FormPage verifyModal() {
         modalComponent.verifyModal();
         return this;
     }
 
+    @Step("Verify modal results")
     public FormPage verifyModalResults(String key, String value) {
         modalComponent.verifyModalResults(key, value);
         return this;
     }
 
+    @Step("Close modal")
     public FormPage closeModal() {
         modalComponent.closeModal();
         return this;
     }
 
-
     public void formValidation() {
-
         $("#closeLargeModal").click();
     }
 }
--- src/test/java/com/tarasov/demoqaproject/tests/TestBase.java
+++ src/test/java/com/tarasov/demoqaproject/tests/TestBase.java
@@ -1,8 +1,14 @@
 package com.tarasov.demoqaproject.tests;
 
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import com.tarasov.demoqaproject.helpers.Attach;
 import com.tarasov.demoqaproject.pages.FormPage;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.openqa.selenium.remote.DesiredCapabilities;
 
 public abstract class TestBase {
     FormPage formPage = new FormPage();
@@ -12,7 +18,27 @@ public abstract class TestBase {
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.browser = "chrome";
         Configuration.browserSize = "1980x1020";
-        Configuration.headless = false;
+        Configuration.browserVersion = "100.0";
+        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
         Configuration.holdBrowserOpen = false;
+        Configuration.headless = false;
+
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("enableVNC", true);
+        capabilities.setCapability("enableVideo", true);
+        Configuration.browserCapabilities = capabilities;
+    }
+
+    @BeforeEach
+    public void addListener() {
+        SelenideLogger.addListener("allure", new AllureSelenide());
+    }
+
+    @AfterEach
+    public void tearDown() {
+        Attach.screenShotAs("Last screenshot");
+        Attach.pageSource();
+        Attach.browserConsoleLogs();
+        Attach.addVideo();
     }
 }
