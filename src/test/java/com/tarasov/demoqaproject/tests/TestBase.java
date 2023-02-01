ba0321cb1830e79a8a0574749003b9f6a8ab1895
evgenyTarasovRepo <eatarasov@yahoo.com>
2023-02-01 20:35:57 +0300

    added system properties in TestBase

--- src/test/java/com/tarasov/demoqaproject/tests/TestBase.java
+++ src/test/java/com/tarasov/demoqaproject/tests/TestBase.java
@@ -16,12 +16,11 @@ public abstract class TestBase {
     @BeforeAll
     public static void setUpAll() {
         Configuration.baseUrl = "https://demoqa.com";
-        Configuration.browser = "chrome";
-        Configuration.browserSize = "1980x1020";
-        Configuration.browserVersion = "100.0";
-        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+        Configuration.browser = System.getProperty("browser", "chrome");
+        Configuration.browserSize = System.getProperty("browserSize", "1980x1020");
+        Configuration.browserVersion = System.getProperty("browser_version", "100.0");
+        Configuration.remote = System.getProperty("remote_url", "https://user1:1234@selenoid.autotests.cloud/wd/hub");
         Configuration.holdBrowserOpen = false;
-        Configuration.headless = false;
 
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability("enableVNC", true);
