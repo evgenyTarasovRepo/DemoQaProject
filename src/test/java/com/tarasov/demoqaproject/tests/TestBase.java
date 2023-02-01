d3785fde7872ca6f49b841d77872ae2f1cdac2e0
evgenyTarasovRepo <eatarasov@yahoo.com>
2023-02-01 21:46:12 +0300

    update testBase

--- src/test/java/com/tarasov/demoqaproject/tests/TestBase.java
+++ src/test/java/com/tarasov/demoqaproject/tests/TestBase.java
@@ -18,8 +18,9 @@ public abstract class TestBase {
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.browser = System.getProperty("browser", "chrome");
         Configuration.browserSize = System.getProperty("browserSize", "1980x1020");
-        Configuration.browserVersion = System.getProperty("browser_version", "100.0");
-        Configuration.remote = System.getProperty("remote_url", "https://user1:1234@selenoid.autotests.cloud/wd/hub");
+        Configuration.browserVersion = System.getProperty("browserVersion", "100.0");
+        Configuration.remote = System.getProperty("remote" +
+                "", "https://user1:1234@selenoid.autotests.cloud/wd/hub");
         Configuration.holdBrowserOpen = false;
 
         DesiredCapabilities capabilities = new DesiredCapabilities();
